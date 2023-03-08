345c5673e393bbdc9f379fce9d70a741f2a5639b
Danylo Kupriienko <79247603+dakup101@users.noreply.github.com>
2023-03-08 18:53:22 +0100

    Info page

---
+++ views/page-info.php
@@ -0,0 +1,32 @@
+<?php
+// Template Name: Info
+?>
+
+<?php get_header(); ?>
+<section class="gallery-intro">
+    <div class="container">
+        <?php get_template_part(THEME_CMP_CMN, "text-title", array(
+            "title" => get_the_title(),
+            "subtitle" => null,
+            "alignment" => "center",
+            "tag" => "h1"
+        )) ?>
+        <div class="gallery-intro-desc">
+            <?php echo get_field("info_desc") ?>
+        </div>
+    </div>
+</section>
+<section class="container info">
+    <?php foreach (get_field("repeat_info") as $repeater) : ?>
+        <?php if ($counter % 2 == 0) : ?>
+            <?php get_template_part(THEME_CMP, "info-right", $repeater["info"]) ?>
+
+        <?php else : ?>
+            <?php get_template_part(THEME_CMP, "info-left", $repeater["info"]) ?>
+
+        <?php endif; ?>
+    <?php $counter++;
+    endforeach; ?>
+</section>
+
+<?php get_footer(); ?>

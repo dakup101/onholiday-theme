bc697aca7139391f79958d04b19943a18ed0db31
Danylo Kupriienko <79247603+dakup101@users.noreply.github.com>
2023-05-16 17:50:42 +0200

    Gallery sections id

--- views/page-gallery.php
+++ views/page-gallery.php
@@ -17,39 +17,35 @@
     </div>
 </section>
 
-<section>
+<?php foreach (get_field("galleries") as $galleries) : ?>
+<section id="<?php echo !empty(get_field("section_id")) ? get_field("section_id") : "" ?>">
     <div class="container">
-        <?php foreach (get_field("galleries") as $galleries) : ?>
-            <?php get_template_part(THEME_CMP_CMN, "text-title", array(
+        <?php get_template_part(THEME_CMP_CMN, "text-title", array(
                 "title" => $galleries["title"],
                 "subtitle" => null,
                 "alignment" => "center",
                 "tag" => "h2",
                 "class" => "font-alt gallery-title"
             )) ?>
-            <div class="gallery">
-                <?php foreach ($galleries["gallery"] as $img) : ?>
-                    <a href="<?php echo $img["url"] ?>" class="glightbox gallery-item">
-                        <img src="<?php echo $img["url"] ?>" alt="<?php echo $img["caption"] ?>" class="gallery-img">
+        <div class="gallery">
+            <?php foreach ($galleries["gallery"] as $img) : ?>
+            <a href="<?php echo $img["url"] ?>"
+               class="glightbox gallery-item">
+                <img src="<?php echo $img["url"] ?>"
+                     alt="<?php echo $img["caption"] ?>"
+                     class="gallery-img">
 
-                    </a>
-                <?php endforeach; ?>
-            </div>
-            <?php if (!empty($galleries["desc"])) : ?>
-            <div class="gallery-desc">
-                <?php echo $galleries["desc"] ?>
-            </div>
-            <?php endif; ?>
-        <?php endforeach; ?>
+            </a>
+            <?php endforeach; ?>
+        </div>
+        <?php if (!empty($galleries["desc"])) : ?>
+        <div class="gallery-desc">
+            <?php echo $galleries["desc"] ?>
+        </div>
+        <?php endif; ?>
     </div>
 </section>
+<?php endforeach; ?>
+
 <?php get_template_part(THEME_CMP, "icons-row") ?>
-<?php // get_template_part(THEME_CMP, "cta", get_field("cta")) 
-?>
-<?php // get_template_part(THEME_CMP, "info-right", get_field("info-right")) 
-?>
-<?php // get_template_part(THEME_CMP, "info-left", get_field("info-left")) 
-?>
-<?php // get_template_part(THEME_CMP, "blog-grid", get_field("blog_grid_fields", "options")) 
-?>
 <?php get_footer(); ?>
